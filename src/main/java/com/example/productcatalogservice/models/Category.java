82838bf9df76320b044cbfb4070245523dc73859
Bhoomika <emmadi.bhoomika@gmail.com>
2025-01-15 13:45:03 +0530

    Fetch types and Fetch modes

--- src/main/java/com/example/productcatalogservice/models/Category.java
+++ src/main/java/com/example/productcatalogservice/models/Category.java
@@ -1,9 +1,12 @@
 package com.example.productcatalogservice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -13,6 +16,7 @@ import java.util.List;
 public class Category extends BaseModel{
     private String name;
     private String description;
-    @OneToMany(mappedBy = "category")
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY) // fetch type: eager, lazy
+    @Fetch(FetchMode.JOIN) // fetch mode: select, join, subselect for more refere to Fetch-Type-Mode-Results file in the same proj location
     private List<Product> products;
 }
